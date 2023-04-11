9ae524fe26167e8de85e166d8204990a16c0cb11
killeen31 <j31killeen@gmail.com>
2023-04-10 22:28:52 -0400

    updates to apiRoutes and added a store.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const path = require('path');
 const fs = require('fs')
+const store = require('../db/store')
 // make a variable that holds require('../db/db.json)
 var db = require('../db/db.json')
 
@@ -12,6 +13,18 @@ router.get('/notes', async (req, res) => {
 })
 
 // POST that creates a note from the req.body and then writes to the db file (fs.writefFileSync) - /api/notes
+router.post('/notes', (req, res) => {
+    store 
+    .addNote(req.body)
+    .then(notes => {
+        res.json(notes)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+   
+    // fs.readFile('../db/db.json').then(data => fs.writeFile)
+})
 // If you are going to do delte note, give that note a id value (body.id = something, maybe db.length)
 // if you want to use a generated id, look into UUIDV4, you will need to npm i uuid
 
